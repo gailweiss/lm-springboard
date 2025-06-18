a70550ca68de3604f793c08ef5fae8601b7165cd
Gail Weiss <gail_weiss@msn.com>
2025-06-18 02:01:52 +0200

    fix index off by one in beforesubseqmasker. leave flow for printing its results if need to check again later

--- data/support.py
+++ data/support.py
@@ -1,3 +1,5 @@
+from misc.util import printer_print as print
+
 class NoMask:
     def __init__(self):
         pass
@@ -5,15 +7,22 @@ class NoMask:
         pass
     def __call__(self, indices):
         return None
+    def __repr__(self):
+        return "nomask"
 
 nomask = NoMask()
 
 class RawSample:
     # for use in creating datasets, before processing in the datamodule
-    def __init__(self, seq, lang=None, target_masker=nomask):
+    def __init__(self, seq, lang=None, note=None, target_masker=nomask):
         self.seq = seq
         self.lang = lang
+        self.note = note
         self.target_masker = target_masker
+
+    def __repr__(self):
+        d = {a: getattr(self, a) for a in ["seq", "lang", "note", "target_masker"]}
+        return f"RawSample({d})"
 
 
 class TokenizedSample:
@@ -32,12 +41,27 @@ class BeforeSubSeqMasker:
     def __init__(self, subseq):
         self.subseq = subseq
         self.prepped = False
+        self.num_calls = 0
 
     def prep(self, tokenizer):
         self.tokenizer = tokenizer
 
+    def show_results(self, indices, res):
+        if self.num_calls > 4:
+            # exit(0)
+            return
+        self.num_calls += 1
+        print("masker showing results")
+        print(self.num_calls)
+        print("input:", self.tokenizer.convert_ids_to_nice_string(indices))
+        mindices = [0 if m else i for i, m in zip(indices, res)]
+        print("masked input:",
+              self.tokenizer.convert_ids_to_nice_string(mindices))
+        print("masked input tokens:",
+              self.tokenizer.convert_ids_to_tokens(mindices))
+
     def __call__(self, indices):
-        def get_index_containing():
+        def get_n_tocontain():
             TOO_LOW, COULD_BE = 1, 2
             def check_i(i):
                 s = self.tokenizer.convert_ids_to_nice_string(indices[:i])
@@ -61,5 +85,10 @@ class BeforeSubSeqMasker:
                     else:
                         hi = i
             return len(indices)  # not found
-        len_to_contain = get_index_containing() + 1
-        return ([1] * len_to_contain) + ([0] * (len(indices) - len_to_contain))
+        len_to_contain = get_n_tocontain()
+        res = ([1] * len_to_contain) + ([0] * (len(indices) - len_to_contain))
+        # self.show_results(indices, res)
+        return res
+
+    def __repr__(self):
+        return f"BeforeSubSeqMasker: {self.subseq}"
